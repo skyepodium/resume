5ccd281dd1b37eb38417db33e2e72c19b8678c5d
skyepodium <skyepodium@gmail.com>
2021-07-25 22:05:42 +0900

    build

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/resume/'
-    : '/',
-	outputDir: process.env.NODE_ENV === 'production'
-    ? './docs'
-    : './dist'    
-}
